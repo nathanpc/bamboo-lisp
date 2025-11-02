89bb8fdc9ab0731da35b9e5a584610b2e4bc3da7
Nathan Campos <nathan@innoveworkshop.com>
2025-11-02 12:08:22 +0000

    Added some color to the make output
    Added some color to the echos in the Makefiles in order to better
    visualize the build process.

--- variables.mk
+++ variables.mk
@@ -6,6 +6,13 @@
 # Environment
 PLATFORM := $(shell uname -s)
 
+# Fancy colors.
+COLOR_RED    := \033[0;31m
+COLOR_GREEN  := \033[0;32m
+COLOR_BLUE   := \033[0;34m
+COLOR_YELLOW := \033[0;33m
+COLOR_END    := \033[0m
+
 # Tools
 AR    = ar
 CC    = gcc
